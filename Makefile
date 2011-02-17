d3485e776e3ebb0dfe9d694b03f545dbcae1ccda
Ben Standefer <benstandefer@gmail.com>
2011-02-16 18:09:17 -0800

    New, merged Python client. First cut merged python-simplegeo-shared with pytho-simplegeo-context.

---
+++ Makefile
@@ -0,0 +1,72 @@
+BASE        ?= $(PWD)
+BIN          = $(BASE)/bin
+PYVERS      := 2.6
+PYTHON       = $(BIN)/python
+PLATFORM     = $(shell uname -s)
+EZ_INSTALL  := $(BIN)/easy_install
+VIRTUALENV  ?= VIRTUALENV_USE_SETUPTOOLS=1 \
+               $(shell test -x `which virtualenv` && which virtualenv || \
+                       test -x `which virtualenv-$(PYVERS)` && \
+                                which virtualenv-$(PYVERS))
+VIRTUALENV  += --no-site-packages
+SETUP        = $(PYTHON) setup.py
+GIT_HEAD     = $(BASE)/.git/$(shell cut -d\  -f2-999 .git/HEAD)
+BUILD_NUMBER ?= 0000INVALID
+
+.PHONY: dev env clean xclean extraclean bundles \
+        debian/changelog
+
+sdist:
+	$(SETUP) sdist
+
+env: $(PYTHON)
+
+$(PYTHON) $(BIN)/easy_install:
+	$(VIRTUALENV) .
+
+dev: env
+	$(SETUP) develop
+
+	@echo "        ==============================================================="
+	@echo "              To activate your shiny new environment, please run:"
+	@echo
+	@which figlet > /dev/null && figlet -c " . bin/activate" || \
+        echo "                                 . bin/activate"
+
+	@echo "        ==============================================================="
+
+lint flakes:
+	$(SETUP) flakes
+
+coverage: .coverage
+	@$(COVERAGE) html -d $@ $(COVERED)
+
+coverage.xml: .coverage
+	@$(COVERAGE) xml $(COVERED)
+
+.coverage: $(SOURCES) $(TESTS)
+	-@$(COVERAGE) run --branch setup.py test -s simplegeo.test
+
+bin/coverage: bin/easy_install
+	@$(EZ_INSTALL) coverage
+
+prereqs: prereqs-$(PLATFORM)
+prereqs-Linux:
+	sudo apt-get install python2.6 python2.6-dev python-virtualenv \
+                         libxml2-dev libxslt-dev libmemcached-dev \
+                         mercurial darcs build-essential g++ wget
+
+prereqs-Darwin:
+	sudo port install python_select \
+                      python26 py26-virtualenv thrift libxml2 libxslt \
+                      libmemcached mercurial darcs wget
+	sudo python_select python26
+
+clean:
+	find . -type f -name \*.pyc -exec rm {} \;
+	rm -rf *.egg *.egg-info htmlcov build* src* dist coverage \
+           build-bundle* _trial_temp
+
+xclean: extraclean
+extraclean: clean
+	rm -rf .Python bin include lib
---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os, re
+
+PKG='simplegeo'
+VERSIONFILE = os.path.join('simplegeo', '_version.py')
+verstr = "unknown"
+try:
+    verstrline = open(VERSIONFILE, "rt").read()
+except EnvironmentError:
+    pass # Okay, there is no version file.
+else:
+    MVSRE = r"^manual_verstr *= *['\"]([^'\"]*)['\"]"
+    mo = re.search(MVSRE, verstrline, re.M)
+    if mo:
+        mverstr = mo.group(1)
+    else:
+        print "unable to find version in %s" % (VERSIONFILE,)
+        raise RuntimeError("if %s.py exists, it must be well-formed" % (VERSIONFILE,))
+    AVSRE = r"^auto_build_num *= *['\"]([^'\"]*)['\"]"
+    mo = re.search(AVSRE, verstrline, re.M)
+    if mo:
+        averstr = mo.group(1)
+    else:
+        averstr = ''
+    verstr = '.'.join([mverstr, averstr])
+
+setup_requires = []
+tests_require = ['mock']
+
+# nosetests is an optional way to get code-coverage results. Uncomment
+# the following and run "python setup.py nosetests --with-coverage.
+# --cover-erase --cover-tests --cover-inclusive --cover-html"
+# tests_require.extend(['coverage', 'nose'])
+
+# trialcoverage is another optional way to get code-coverage
+# results. Uncomment the following and run "python setup.py trial
+# --reporter=bwverbose-coverage -s simplegeo.test".
+# setup_requires.append('setuptools_trial')
+# tests_require.extend(['setuptools_trial', 'trialcoverage'])
+
+# As of 2010-11-22 neither of the above options appear to work to
+# generate code coverage results, but the following does:
+# rm -rf ./.coverage* htmlcov ; coverage run --branch  --include=simplegeo/* setup.py test ; coverage html
+
+setup(name=PKG,
+      version=verstr,
+      description="Library for interfacing with SimpleGeo's API",
+      author="Zooko Wilcox-O'Hearn and Ben Standefer",
+      author_email="ben@simplegeo.com",
+      url="http://github.com/simplegeo/python-simplegeo",
+      packages = find_packages(),
+      license = "MIT License",
+      install_requires=['httplib2>=0.6.0', 'oauth2>=1.5', 'pyutil[jsonutil] >= 1.8.1', 'ipaddr >= 2.0.0'],
+      keywords="simplegeo",
+      zip_safe=False, # actually it is zip safe, but zipping packages doesn't help with anything and can cause some problems (http://bugs.python.org/setuptools/issue33 )
+      namespace_packages = ['simplegeo'],
+      test_suite='simplegeo.test',
+      setup_requires=setup_requires,
+      tests_require=tests_require)
