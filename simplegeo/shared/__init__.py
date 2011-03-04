b05629b1eebe2de7ba2072386755554694f3465d
Ben Standefer <benstandefer@gmail.com>
2011-03-03 17:25:31 -0800

    Add backwards compatibility with simplegeo.shared.

---
+++ simplegeo/shared/__init__.py
@@ -0,0 +1,3 @@
+# This is here for backwards compatibility only.
+from simplegeo.util import json_decode, swap, deep_swap, deep_validate_lat_lon, SIMPLEGEOHANDLE_RSTR, SIMPLEGEOHANDLE_R, is_simplegeohandle, is_numeric, is_valid_lat, is_valid_lon, to_unicode, APIError, DecodeError
+from simplegeo.models import Feature
