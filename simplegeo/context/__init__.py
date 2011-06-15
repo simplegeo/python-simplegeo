297234c7279ec5909d90cefc0508091b75c246bc
Andrew Mager <andrew.mager@gmail.com>
2011-06-15 13:21:48 -0700

    Added ability to filter in SimpleGeo Context client

--- simplegeo/context/__init__.py
+++ simplegeo/context/__init__.py
@@ -21,40 +21,78 @@ class Client(ParentClient):
 
         self.endpoints.update(map(lambda x: (x[0], api_version+x[1]), context_endpoints))
 
-    def get_context(self, lat, lon):
+    def get_context(self, lat, lon, filter=None):
         _assert_valid_lat(lat)
         _assert_valid_lon(lon)
+        
+        if (filter and not isinstance(filter, basestring)):
+            raise ValueError("Query must be a string.")
+        
+        kwargs = { }
+        if filter:
+            kwargs['filter'] = filter
+        
         endpoint = self._endpoint('context', lat=lat, lon=lon)
-        return json_decode(self._request(endpoint, "GET")[1])
+        result = self._request(endpoint, 'GET', data=kwargs)[1]
+        return json_decode(result)
 
-    def get_context_by_ip(self, ipaddr):
+    def get_context_by_ip(self, ipaddr, filter=None):
         """ The server uses guesses the latitude and longitude from
         the ipaddr and then does the same thing as get_context(),
         using that guessed latitude and longitude."""
+
         if not is_valid_ip(ipaddr):
             raise ValueError("Address %s is not a valid IP" % ipaddr)
-        endpoint = self._endpoint('context_by_ip', ip=ipaddr)
-        return json_decode(self._request(endpoint, "GET")[1])
+        if (filter and not isinstance(filter, basestring)):
+            raise ValueError("Query must be a string.")
+        
+        kwargs = { }
+        if filter:
+            kwargs['filter'] = filter
 
-    def get_context_by_my_ip(self):
+        endpoint = self._endpoint('context_by_ip', ip=ipaddr)
+        result = self._request(endpoint, 'GET', data=kwargs)[1]
+        return json_decode(result)
+
+    def get_context_by_my_ip(self, filter=None):
         """ The server gets the IP address from the HTTP connection
         (this may be the IP address of your device or of a firewall,
         NAT, or HTTP proxy device between you and the server), and
         then does the same thing as get_context_by_ip(), using that IP
         address."""
-        endpoint = self._endpoint('context_by_my_ip')
-        return json_decode(self._request(endpoint, "GET")[1])
 
-    def get_context_by_address(self, address):
+        if (filter and not isinstance(filter, basestring)):
+            raise ValueError("Query must be a string.")
+        
+        kwargs = { }
+        if filter:
+            kwargs['filter'] = filter
+
+        endpoint = self._endpoint('context_by_my_ip')
+        result = self._request(endpoint, 'GET', data=kwargs)[1]
+        return json_decode(result)
+
+    def get_context_by_address(self, address, filter=None):
         """
         The server figures out the latitude and longitude from the
         street address and then does the same thing as get_context(),
         using that deduced latitude and longitude.
         """
+        
         if not isinstance(address, basestring):
             raise ValueError("Address must be a string.")
+        if (filter and not isinstance(filter, basestring)):
+            raise ValueError("Query must be a string.")
+        
+        kwargs = { }
+        if filter:
+            kwargs['filter'] = filter
+        if address:
+            kwargs['address'] = address
+
         endpoint = self._endpoint('context_by_address')
-        return json_decode(self._request(endpoint, "GET", data={'address' : address})[1])
+        result = self._request(endpoint, 'GET', data=kwargs)[1]
+        return json_decode(result)
 
 
 
